8f5822a20db89360cefc4d65bbf525521d5e0407
TheLulGuy <ombanerjee10@gmail.com>
2021-05-09 22:33:19 +0530

    changed tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,28 @@
 import random
 
-def print_board(board):
+board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+
+def print_board():
     print(' {} | {} | {} '.format(board[0], board[1], board[2]))
     print('---+---+---')
     print(' {} | {} | {} '.format(board[3], board[4], board[5]))
     print('---+---+---')
     print(' {} | {} | {} '.format(board[6], board[7], board[8]))
+
+def takeInputs():
+    while True:
+        answer = ''
+        try:
+            answer = int(input('Chose a index to put your piece(1-9): '))
+            if answer > 9 or answer < 1:
+                raise ValueError
+            
+            return answer
+        except ValueError:
+            print('Invalid input, try again')
+            continue
+
+
 
 def greeting():
     while True:
@@ -17,13 +34,33 @@ def greeting():
             print("Then y u waste my time")
             exit(1)
 
+def boardFull():
+    for item in board:
+        if item == ' ':
+            return False
+    
+    return True
+def compTurn():
+    return random.randint(0, 9)
+
+def replace(index, item, sub=False):
+    if board[index] != ' ':
+        if sub == False:
+            board[index] = item
+        else:
+            board[index - 1] = item
+    else:
+        return -1
 
 
 def play():
-    board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',]
     playerTurn = random.choice([True, False])
+    game_still_going_on = True
     greeting()
+    while game_still_going_on:
+        if playerTurn == True:
+            replace(takeInputs(), 'X', sub=True)
+        else:
+            # playerTurn == False
+            replace(compTurn(), 'O')
 
-
-if __name__ == '__main__':
-    play()
