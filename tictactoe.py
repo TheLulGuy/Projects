99f8c0e5942553846ddca5b4da88d96d86abf8a8
TheLulGuy <ombanerjee10@gmail.com>
2021-05-10 12:13:17 +0530

    tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -14,7 +14,6 @@ def takeInputs():
             answer = int(input('Chose a index to put your piece(1-9): '))
             if answer > 9 or answer < 1:
                 raise ValueError
-            
             return answer
         except ValueError:
             print('Invalid input, try again')
@@ -23,25 +22,25 @@ def takeInputs():
 def checkIfWon():
     # rows
     if board[0] == board[1] == board[2]:
-        return True
+        return True, board[0]
     if board[3] == board[4] == board[5]:
-        return True
+        return True, board[4]
     if board[6] == board[7] == board[8]:
-        return True
+        return True, board[8]
     
     # colums
     if board[0] == board[3] == board[6]:
-        return True
+        return True, board[0]
     if board[1] == board[4] == board[7]:
-        return True
+        return True, board[4]
     if board[2] == board[5] == board[8]:
-        return True
+        return True, board[8]
     
     # diagonals
     if board[0] == board[4] == board[8]:
-        return True
+        return True, board[4]
     if board[2] == board[4] == board[6]:
-        return True
+        return True, board[4]
     
     return False
     
@@ -68,19 +67,23 @@ def compTurn():
 def replace(index, item, sub=False):
     if sub == True:
         index -= 1
-
-
+    
+    if board[index] == ' ':
+        board[index] = item
 
 def play():
     global board
     global game_still_going_on
     global takenSpots
+    winning = ''
     board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
     game_still_going_on = True
     playerTurn = random.choice([True, False])
     greeting()
     while game_still_going_on:
+        game_still_going_on, winning = checkIfWon()
         if playerTurn == True:
+            print_board()
             replace(takeInputs(), 'X', sub=True)
             playerTurn = False
             continue
@@ -89,4 +92,11 @@ def play():
             replace(compTurn(), 'O')
             playerTurn = True
             continue
+    
+    if not game_still_going_on:
+        if winning == 'X':
+            print('YOU WON')
+        else:
+            print('YOU LOST')
 
+play()
