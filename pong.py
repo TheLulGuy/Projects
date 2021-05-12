beeb32f6e67741d59c3bdccdb9265c4099bc29ed
TheLulGuy <ombanerjee10@gmail.com>
2021-05-12 17:13:49 +0530

    added functionality to paddles

--- pong.py
+++ pong.py
@@ -28,10 +28,13 @@ paddle_b.goto(350, 0)
 # Ball
 ball = turtle.Turtle()
 ball.speed(0) 
-ball.shape("circle")
+ball.shape("square")
 ball.color('white')
 ball.penup()
 ball.goto(0, 0)
+# This basically means our ball moves by 2 pixels everytime
+ball.dx = 3 # Delta x or change in x
+ball.dy = -3
 
 def paddle_a_up():
     y = paddle_a.ycor()
@@ -64,3 +67,29 @@ window.onkeypress(paddle_b_down, "Down")
 # Main game loop
 while True:
     window.update()
+    # Move ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+    
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx = -1.5
+    
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx = -1.5
+
+    # Paddle and ball collision
+
+    if (ball.xcor() > 345 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() < -40):
+        ball.setx(340)
+        ball.dx *= -1
