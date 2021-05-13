13e6b1fe8a7a1b95dfc87c1a6c8b9dd46b82794e
TheLulGuy <ombanerjee10@gmail.com>
2021-05-13 10:58:42 +0530

    completed pong, with audio

--- pong.py
+++ pong.py
@@ -1,4 +1,5 @@
 import turtle
+import os
 
 window = turtle.Screen()
 window.title("Pong with Hom")
@@ -7,6 +8,9 @@ window.setup(800, 600)
 # This makes th game update manually, to speed up the performance
 window.tracer()
 
+# Score variables
+score_a = 0
+score_b = 0
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0) # Max animation speed
@@ -28,13 +32,23 @@ paddle_b.goto(350, 0)
 # Ball
 ball = turtle.Turtle()
 ball.speed(0) 
-ball.shape("square")
+ball.shape("circle")
 ball.color('white')
 ball.penup()
 ball.goto(0, 0)
 # This basically means our ball moves by 2 pixels everytime
-ball.dx = 3 # Delta x or change in x
-ball.dy = -3
+ball.dx = 2 # Delta x or change in x
+ball.dy = 2
+
+# Score printer
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color('white')
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write('Player A: 0   Player B: 0', align='center', font=('Courier', 24, 'normal'))
 
 def paddle_a_up():
     y = paddle_a.ycor()
@@ -75,21 +89,37 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        os.system('afplay bounceAudio.wav&')
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        os.system('afplay bounceAudio.wav&')
+
     
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx = -1.5
+        score_a += 1
+        pen.clear()
+        pen.write(f'Player A: {score_a}   Player B: {score_b}', align='center', font=('Courier', 24, 'normal'))
     
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx = -1.5
+        score_b += 1
+        pen.clear()
+        pen.write(f'Player A: {score_a}   Player B: {score_b}', align='center', font=('Courier', 24, 'normal'))
+
 
     # Paddle and ball collision
 
-    if (ball.xcor() > 345 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() < -40):
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
+        os.system('afplay bounceAudio.wav&')
+    
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1    
+        os.system('afplay bounceAudio.wav&')
