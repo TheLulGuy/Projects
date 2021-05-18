7679ab459e11454ea996653e901a7ded75395506
TheLulGuy <ombanerjee10@gmail.com>
2021-05-18 20:41:44 +0530

    abandoned snake project

--- snake(ABANDONED).py
+++ snake(ABANDONED).py
@@ -4,9 +4,11 @@ import math
 import random
 import pygame
 
+
 class cube(object):
     rows = 0
     w = 0
+
     def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
         pass
 
@@ -15,6 +17,7 @@ class cube(object):
 
     def draw(self, surface, eyes=False):
         pass
+
 
 class snake(object):
     def __init__(self, color, pos):
@@ -32,28 +35,46 @@ class snake(object):
     def draw(self, surface):
         pass
 
+
 def drawGrid(w, rows, surface):
-    pass
+    sizeBtwn = w // rows
+    x, y = 0, 0
+    for i in range(rows):
+        x += sizeBtwn
+        y += sizeBtwn
+    
+        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
+        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
+        
 
 def redrawWindow(surface):
-    pass
+    global width, rows
+    width = 200
+    rows = 50
+    fill((0, 0, 0))
+    drawGrid(width, rows, surface)
+    pygame.display.update()
+
 
 def randomSnack(rows, items):
     pass
 
+
 def message_box(subject, content):
     pass
+
 
 def main():
     # It should be the same so that we have the same no. of boxes
     window = pygame.display.set_mode((500, 500))
-    rows = 20 # divisible by 500, thus makes 25x25 grid
+    rows = 20  # divisible by 500, thus makes 25x25 grid
     s = snake((255, 0, 0), (10, 10))
 
     clock = pygame.time.Clock()
     while True:
-        pygame.time.delay(50) # milliseconds
-        clock.tick(10) # This makes it so that the snake doesnt zoom around the board
+        pygame.time.delay(50)  # milliseconds
+        # This makes it so that the snake doesnt zoom around the board
+        clock.tick(10)
         redrawWindow(surface=window)
 
 
@@ -64,4 +85,3 @@ cube.rows = rows
 cube.w = w
 
 main()
-
