11e32d67e3eb7ce554813a2795a3278142ad0826
TheLulGuy <ombanerjee10@gmail.com>
2023-11-27 19:23:25 +0530

    Organized workplace into different languages

--- textSpammer.py
+++
@@ -1,9 +0,0 @@
-import pyautogui, time
-
-msg = input('Enter message: ')
-n = input('Enter how many times you want to repeat the message: ')
-print('\nNow quickly switch over to your messaging app!')
-time.sleep(5)
-for item in range(int(n)):
-    pyautogui.typewrite(msg)
-    pyautogui.press('enter')
